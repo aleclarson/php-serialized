afa52d16d2a56d482dd5056a95e90b1f4b3da4d4
aleclarson <alec.stanford.larson@gmail.com>
2018-08-18 11:22:42 -0400

    stop converting arrays in `reduce`

--- reduce.js
+++ reduce.js
@@ -5,15 +5,6 @@ function reduce(ast) {
 }
 
 function reduceNode(node) {
-  if (node.type == 'array') {
-    let map = new Map;
-    let nodes = node.value;
-    for (let i = 0; i < nodes.length; i += 2) {
-      let key = nodes[i], val = nodes[i + 1];
-      map.set(reduceNode(key), reduceNode(val));
-    }
-    return map;
-  }
   if (node.type == 'error') {
     let e = new Error(node.value);
     e.start = node.start;
